7e0af81ecca0445c0710df01d7d71e0bcf9ba63f
Nick <ntambroson@dmacc.edu>
2021-06-30 21:09:33 -0500

    Add project files.

---
+++ Models/FirstResponsiveWebAppModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ch03FirstResponsiveWebAppAmbroson.Models
+{
+    public class FirstResponsiveWebAppModel
+    {
+        [Required(ErrorMessage = "Please enter your name.")]
+        [RegularExpression("^[A-Za-z](([A-Za-z])|([A-Za-z]['-](?=[A-Za-z]))|( (?=[A-Za-z])))*$", ErrorMessage = "Name must only contain letters, hyphens, and apostrophes.")]
+        public string Name { get; set; }
+
+
+        [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+        public class YearRange : RangeAttribute
+        {
+            public YearRange(int minimum) : base(minimum, DateTime.Now.Year)
+            {
+            }
+        }
+
+        [YearRange(1900, ErrorMessage = "Please enter a valid birth year")]
+        [Required(ErrorMessage = "Please enter your birth year")]
+        public int BirthYear { get; set; }
+
+        public int AgeThisYear()
+        {
+            int birthYear = BirthYear;
+            DateTime currentYear = DateTime.Today;
+            int currentAge;
+
+            currentAge = currentYear.Year - birthYear;
+
+            return currentAge;
+        }
+    }
+}
